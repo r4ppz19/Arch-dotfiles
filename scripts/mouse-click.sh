f70b1d3d9531ab06a548c2b497ae1a6004c928c6
r4ppz <johnreyrabosa.f@gmail.com>
2025-06-02 14:53:20 +0800

    fix(mouse-click): correct typo in 'toggle' case statement

--- scripts/mouse-click.sh
+++ scripts/mouse-click.sh
@@ -9,7 +9,7 @@ case "$1" in
   left) /usr/bin/ydotool click 0x40 0x80 ;;
   right) /usr/bin/ydotool click 0x41 0x81 ;;
   middle) /usr/bin/ydotool click 0x42 0x82 ;;
-  toogle)
+  toggle)
     STATE_FILE="/tmp/mouse_state"
 
     if [ ! -f "$STATE_FILE" ]; then
