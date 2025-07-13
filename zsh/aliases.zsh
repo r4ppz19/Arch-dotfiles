3e7a55b091fa729a6f2b4cc517759b6ca311780b
r4ppz <johnreyrabosa.f@gmail.com>
2025-07-13 17:35:18 +0800

    fix(alias): update ts => ti

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -27,7 +27,7 @@ alias sm='~/Arch-dotfiles/scripts/tmux-session-manager.py'
 alias ssh='TERM=xterm-256color ssh'
 alias top='btop'
 alias of="~/Arch-dotfiles/scripts/open-file.sh"
-alias ts="~/Arch-dotfiles/scripts/tmux-init.sh"
+alias ti="~/Arch-dotfiles/scripts/tmux-init.sh"
 alias fs="sudo systemctl start smb.service nmb.service"
 alias fss="sudo systemctl stop smb.service nmb.service"
 
