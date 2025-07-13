b0ea9d40254833530aa7d94a1e6a91c1239826d6
r4ppz <johnreyrabosa.f@gmail.com>
2025-07-13 19:08:39 +0800

    feat(alias): add music alias for kew and a little bit of refractoring
    change the not needed " to be ' for consistency

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -7,32 +7,35 @@
 # tgpt, neovim, ssh
 
 # Built-in improvements
-alias ls="eza --icons --group-directories-first --color=auto"
-alias la="eza -a --icons --group-directories-first --color=auto"
-alias ll="eza -lh --icons --group-directories-first --color=auto"
-alias tree="eza -T --icons --group-directories-first --color=auto"
-alias less="less -SRX"
-alias mv="mv -v"
-alias cp="cp -v"
-alias mkdir="mkdir -pv"
-alias ..="cd .."
-alias open="xdg-open"
+alias ls='eza --icons --group-directories-first --color=auto'
+alias la='eza -a --icons --group-directories-first --color=auto'
+alias ll='eza -lh --icons --group-directories-first --color=auto'
+alias tree='eza -T --icons --group-directories-first --color=auto'
+alias less='less -SRX'
+alias mv='mv -v'
+alias cp='cp -v'
+alias mkdir='mkdir -pv'
+alias ..='cd ..'
+alias open='xdg-open'
 
 alias compress='tar -czvf'
 alias r='ranger --choosedir="$HOME/.rangerdir"; LASTDIR=$(cat "$HOME/.rangerdir"); cd "$LASTDIR"'
-alias v="nvim"
+alias v='nvim'
 alias lg='lazygit'
-# alias sm='. ~/Arch-dotfiles/zsh/session-manager.zsh'
-alias sm='~/Arch-dotfiles/scripts/tmux-session-manager.py'
-alias ssh='TERM=xterm-256color ssh'
 alias top='btop'
-alias of="~/Arch-dotfiles/scripts/open-file.sh"
-alias ti="~/Arch-dotfiles/scripts/tmux-init.sh"
-alias fs="sudo systemctl start smb.service nmb.service"
-alias fss="sudo systemctl stop smb.service nmb.service"
+alias music='kew'
+alias ssh='TERM=xterm-256color ssh'
+alias smb-start='sudo systemctl start smb.service nmb.service'
+alias smb-stop='sudo systemctl stop smb.service nmb.service'
+
+# script
+alias of='~/Arch-dotfiles/scripts/open-file.sh'
+alias ti='~/Arch-dotfiles/scripts/tmux-init.sh'
+alias sm='~/Arch-dotfiles/scripts/tmux-session-manager.py'
+# alias sm='. ~/Arch-dotfiles/zsh/session-manager.zsh'
 
 # AI
 alias explain='gh copilot explain'
 alias suggest='gh copilot suggest'
-alias gemma3="tgpt --provider ollama --model gemma3"
-alias poll="tgpt --provider pollinations"
+alias gemma3='tgpt --provider ollama --model gemma3'
+alias poll='tgpt --provider pollinations'
