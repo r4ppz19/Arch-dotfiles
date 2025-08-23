d3de82fce72118e6cc663998e53899ffbecccadb
r4ppz <johnreyrabosa.f@gmail.com>
2025-08-23 10:42:15 +0800

    feat(zsh functions): add base conversion, eachf, fd, rg functions
    ff = find all (including .) file in the currect directory
    gg = grep all files in the currect directory (including .)
    eachf = do a command to every single files in the wd
    add all possible base conversions

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -37,7 +37,78 @@ ai() {
 # file search
 ff() {
   local file
-  file=$(fd --type f --hidden . | fzf --preview 'bat --style=numbers --color=always {}' --height=50% --layout=reverse --border)
-  [[ -n "$file" ]] && ${EDITOR:-nvim} "$file"
+  file=$(
+    fd --type f --hidden --no-ignore . \
+      | fzf \
+          --preview 'bat --style=numbers --color=always {} || cat {}' \
+          --preview-window=right:60%:wrap \
+          --height=50% \
+          --layout=reverse \
+          --border \
+          --bind "ctrl-d:change-preview-window(down|50%)" \
+          --bind "ctrl-r:reload(fd --type f --hidden --no-ignore .)"
+  )
+  [[ -n "$file" ]] && ${EDITOR:-vim} "$file"
+}
+
+gg() {
+  local file
+  file=$(
+    rg --hidden --no-ignore --files-with-matches "" . 2>/dev/null \
+      | fzf \
+          --preview 'bat --style=numbers --color=always {} || cat {}' \
+          --preview-window=right:60%:wrap \
+          --height=50% \
+          --layout=reverse \
+          --border \
+          --bind "ctrl-d:change-preview-window(down|50%)" \
+          --bind "ctrl-r:reload(rg --hidden --no-ignore --files-with-matches "" . 2>/dev/null)"
+  )
+  [[ -n "$file" ]] && ${EDITOR:-vim} "$file"
+}
+
+eachf() {
+  find . -type f -exec "$@" {} \;
+}
+
+# Decimal to binary
+dec2bin() {
+  echo "obase=2; $1" | bc
+}
+# Binary to decimal
+bin2dec() {
+  echo "$((2#$1))"
+}
+# Binary to hex
+bin2hex() {
+  printf "%X\n" "$((2#$1))"
+}
+# Hex to binary
+hex2bin() {
+  echo "obase=2; ibase=16; $1" | bc
+}
+# Hex to decimal
+hex2dec() {
+  echo "$((16#$1))"
+}
+# Decimal to hex
+dec2hex() {
+  printf "%X\n" "$1"
+}
+# Decimal to octal
+dec2oct() {
+  printf "%o\n" "$1"
+}
+# Octal to decimal
+oct2dec() {
+  echo "$((8#$1))"
+}
+# Octal to hex
+oct2hex() {
+  printf "%X\n" "$((8#$1))"
+}
+# Hex to octal
+hex2oct() {
+  echo "obase=8; ibase=16; $1" | bc
 }
 
