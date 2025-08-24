4446afe6072310380a4def8765b47253109294e4
r4ppz <johnreyrabosa.f@gmail.com>
2025-08-24 12:48:08 +0800

    feat(websearch): add new file aliases

--- rofi/websearch/websearch.sh
+++ rofi/websearch/websearch.sh
@@ -3,9 +3,14 @@
 declare -A sites=(
     [chat]="https://chatgpt.com/"
     [fb]="https://www.facebook.com"
-    [gh]="https://github.com"
+    [gh]="https://github.com/r4ppz19"
     [yt]="https://www.youtube.com"
     [pins]="https://www.pinterest.com"
+    [mail]="https://mail.google.com/mail/u/0/#inbox"
+    [movie]="https://hurawatchtv.tv/home"
+    [book]="https://annas-archive.org/"
+    [music]="https://open.spotify.com/"
+    [figma]="https://www.figma.com/files/team/1453677715883679951/drafts?fuid=1453677713981521812"
 )
 
 query=$(rofi -dmenu -theme "$HOME/.config/rofi/websearch/main.rasi")
