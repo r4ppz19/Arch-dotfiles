8d474d14f5db40253e92952f3afcf0fd5c438299
r4ppz <johnreyrabosa.f@gmail.com>
2025-08-29 10:52:53 +0800

    feat: add olsis link alias

--- rofi/websearch/websearch.sh
+++ rofi/websearch/websearch.sh
@@ -11,6 +11,7 @@ declare -A sites=(
     [book]="https://annas-archive.org/"
     [music]="https://open.spotify.com/"
     [figma]="https://www.figma.com/files/team/1453677715883679951/drafts?fuid=1453677713981521812"
+    [olsis]="https://tsis.assumptiondavao.edu.ph/"
 )
 
 query=$(rofi -dmenu -theme "$HOME/.config/rofi/websearch/main.rasi")
