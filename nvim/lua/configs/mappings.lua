09b3522ed485ee26d2b4eeb2e275dfec0a354de8
r4ppz <johnreyrabosa.f@gmail.com>
2025-08-30 10:10:35 +0800

    refractor: arrange code

--- nvim/lua/configs/mappings.lua
+++ nvim/lua/configs/mappings.lua
@@ -4,11 +4,10 @@ local map = function(mode, lhs, rhs, opts)
 	vim.keymap.set(mode, lhs, rhs, opts)
 end
 
--- Highlight word under cursor without moving
+-- Editor remaps/ built in
 map("n", "*", [[<Cmd>let @/ = '\<'.expand('<cword>').'\>'<CR>:set hlsearch<CR>]], { desc = "Highlight word (no jump)" })
 map("n", "#", [[<Cmd>let @/ = '\<'.expand('<cword>').'\>'<CR>:set hlsearch<CR>]], { desc = "Highlight word (no jump)" })
 
--- Editor remaps/ built in
 map("i", "<C-h>", "<C-w>", { desc = "Make Ctrl+Backspace act as ctrl+w in insert mode" })
 
 map({ "n", "v" }, "<C-Left>", "b", { desc = "Move to the beginning of the word" })
