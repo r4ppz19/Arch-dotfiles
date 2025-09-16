97418112ac7162f20553a75e0d8d29660d29e3ab
r4ppz <johnreyrabosa.f@gmail.com>
2025-09-16 19:53:21 +0800

    refactor: update CopilotChat configuration and keybindings

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -1,38 +1,60 @@
 return {
-	"CopilotC-Nvim/CopilotChat.nvim",
-	dependencies = {
-		{ "github/copilot.vim" },
-		{ "nvim-lua/plenary.nvim", branch = "master" },
+	{
+		"zbirenbaum/copilot.lua",
+		cmd = "Copilot",
+		event = "InsertEnter",
+		config = function()
+			require("copilot").setup({
+				suggestion = { enabled = false },
+				panel = { enabled = false },
+			})
+		end,
 	},
-	build = "make tiktoken",
 
-	config = function()
-		require("CopilotChat").setup({
+	{
+		"CopilotC-Nvim/CopilotChat.nvim",
+		branch = "main",
+		dependencies = {
+			{ "zbirenbaum/copilot.lua" },
+			{ "nvim-lua/plenary.nvim" },
+		},
+		build = "make tiktoken",
+		opts = {
+			model = "gpt-4o",
+			temperature = 0.1,
 			window = {
 				layout = "vertical",
-				width = 0.3,
+				width = 0.4,
 			},
-		})
-	end,
+			auto_insert_mode = false,
+		},
+		config = function(_, opts)
+			require("CopilotChat").setup(opts)
+		end,
+		keys = {
+			-- Normal mode keybindings
+			{ "<leader>cc", "<cmd>CopilotChatToggle<cr>", desc = "Toggle CopilotChat" },
+			{ "<leader>cr", "<cmd>CopilotChatReset<cr>", desc = "Reset CopilotChat" },
 
-	cmd = {
-		"CopilotChat",
-		"CopilotChatOpen",
-		"CopilotChatClose",
-		"CopilotChatToggle",
-		"CopilotChatStop",
-		"CopilotChatReset",
-		"CopilotChatSave",
-		"CopilotChatLoad",
-		"CopilotChatPrompts",
-		"CopilotChatModels",
-		"CopilotChatAgents",
-	},
-	keys = {
-		{ "<leader>ca", "<cmd>CopilotChatAgents<cr>", desc = "Copilot Agents" },
-		{ "<leader>cp", "<cmd>CopilotChatPrompts<cr>", desc = "Copilot Prompts" },
-		{ "<leader>cm", "<cmd>CopilotChatModels<cr>", desc = "Copilot Models" },
-		{ "<leader>cc", "<cmd>CopilotChatToggle<cr>", mode = "n", desc = "Copilot Chat" },
-		{ "<leader>cc", "<cmd>CopilotChatToggle<cr>", mode = "v", desc = "Copilot Chat" },
+			-- Visual mode keybindings - select text and press these keys
+			{ "<leader>ce", "<cmd>CopilotChatExplain<cr>", mode = "v", desc = "Explain selected code" },
+			{ "<leader>cf", "<cmd>CopilotChatFix<cr>", mode = "v", desc = "Fix selected code" },
+			{ "<leader>co", "<cmd>CopilotChatOptimize<cr>", mode = "v", desc = "Optimize selected code" },
+			{ "<leader>cr", "<cmd>CopilotChatReview<cr>", mode = "v", desc = "Review selected code" },
+			{ "<leader>cd", "<cmd>CopilotChatDocs<cr>", mode = "v", desc = "Add docs to selected code" },
+			{ "<leader>ct", "<cmd>CopilotChatTests<cr>", mode = "v", desc = "Generate tests for selected code" },
+
+			{
+				"<leader>cc",
+				function()
+					local select = require("CopilotChat.select")
+					require("CopilotChat").open({
+						selection = select.visual,
+					})
+				end,
+				mode = "v",
+				desc = "Open chat with selected code as context",
+			},
+		},
 	},
 }
