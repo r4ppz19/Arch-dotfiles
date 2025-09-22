7ec688b0a8f9ab0d8d67f4e5647fcda2778e7ba9
r4ppz <johnreyrabosa.f@gmail.com>
2025-09-22 13:32:26 +0800

    fix!: refractor lsp configuration based on docs

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,206 +1,112 @@
 return {
 	"neovim/nvim-lspconfig",
 	dependencies = {
-		"williamboman/mason-lspconfig.nvim",
-		"williamboman/mason.nvim",
+		{
+			"mason-org/mason.nvim",
+			opts = {},
+		},
+		{
+			"mason-org/mason-lspconfig.nvim",
+			opts = {
+				ensure_installed = {
+					"lua_ls",
+					"pyright",
+					"bashls",
+					"rust_analyzer",
+					"html",
+					"cssls",
+					"emmet_ls",
+					"ts_ls",
+					"eslint",
+					"jsonls",
+				},
+			},
+		},
 		"hrsh7th/cmp-nvim-lsp",
 		"nvim-telescope/telescope.nvim",
 	},
 
 	config = function()
-		require("mason").setup({
-			ui = {
-				border = "single",
-				width = 0.8,
-				height = 0.8,
-				icons = {
-					package_installed = "✓",
-					package_pending = "➜",
-					package_uninstalled = "✗",
-				},
-			},
-		})
-
-		require("mason-lspconfig").setup({
-			ensure_installed = {
-				"lua_ls",
-				"pyright",
-				"bashls",
-				"rust_analyzer",
-				"systemd_ls",
-				"html",
-				"cssls",
-				"emmet_ls",
-				"ts_ls",
-				"eslint",
-				"jsonls",
-				"css_variables",
-				"cssmodules_ls",
-			},
-			automatic_installation = true,
-		})
-
-		local function on_attach(_, bufnr)
-			local map = vim.keymap.set
-			local tb = require("telescope.builtin")
-			local themes = require("telescope.themes")
-
-			-- LSP API
-			map("n", "gd", vim.lsp.buf.definition, { buffer = bufnr, desc = "Goto Definition" })
-			map("n", "gi", vim.lsp.buf.implementation, { buffer = bufnr, desc = "Goto Implementation" })
-			map("n", "gt", vim.lsp.buf.type_definition, { buffer = bufnr, desc = "Goto Type Definition" })
-			map("n", "K", vim.lsp.buf.hover, { buffer = bufnr, desc = "Hover Doc" })
-			map("i", "<C-k>", vim.lsp.buf.signature_help, { buffer = bufnr, desc = "Signature Help" })
-			map("n", "]d", vim.diagnostic.goto_next, { buffer = bufnr, desc = "Next Diagnostic" })
-			map("n", "[d", vim.diagnostic.goto_prev, { buffer = bufnr, desc = "Prev Diagnostic" })
-
-			map("n", "<leader>lh", vim.lsp.buf.signature_help, { buffer = bufnr, desc = "Signature Help" })
-			map("n", "<leader>lq", vim.diagnostic.setloclist, { desc = "Diagnostics: Set Loclist" })
-			map("n", "<leader>ln", vim.lsp.buf.rename, { buffer = bufnr, desc = "Rename Symbol" })
-			map("n", "<leader>la", vim.lsp.buf.code_action, { buffer = bufnr, desc = "Code Action" })
-
-			-- Telescope API
-			map("n", "<leader>ls", tb.lsp_document_symbols, { buffer = bufnr, desc = "LSP Document Symbols" })
-			map("n", "<leader>lS", tb.lsp_workspace_symbols, { buffer = bufnr, desc = "LSP Workspace Symbols" })
-
-			map("n", "<leader>ld", function()
-				tb.diagnostics(themes.get_dropdown({
-					previewer = false,
-					layout_config = {
-						width = 0.7,
-						height = 0.7,
-					},
-					prompt_title = "Diagnostics",
-					include_declaration = true,
-				}))
-			end, { buffer = bufnr, desc = "Diagnostics" })
-
-			map("n", "<leader>lr", function()
-				tb.lsp_references({
-					jump_type = "never",
-				})
-			end, { buffer = bufnr, desc = "LSP References" })
-		end
-
+		-- Set up capabilities for completion
 		local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
+		-- Configure specific servers that need custom settings
 		vim.lsp.config("lua_ls", {
-			on_attach = on_attach,
 			capabilities = capabilities,
 			settings = {
 				Lua = {
-					runtime = { version = "LuaJIT", path = vim.split(package.path, ";") },
+					runtime = { version = "LuaJIT" },
 					diagnostics = { globals = { "vim" } },
-					workspace = { library = vim.api.nvim_get_runtime_file("", true), checkThirdParty = false },
+					workspace = {
+						library = vim.api.nvim_get_runtime_file("", true),
+						checkThirdParty = false,
+					},
 					telemetry = { enable = false },
 				},
 			},
 		})
 
 		vim.lsp.config("rust_analyzer", {
-			on_attach = on_attach,
 			capabilities = capabilities,
 			settings = {
 				["rust-analyzer"] = {
-					inlayHints = {
-						bindingModeHints = false,
-						typeHints = false,
-						parameterHints = false,
-						chainingHints = false,
-					},
 					cargo = { allFeatures = true },
 					check = { command = "clippy" },
 				},
 			},
 		})
 
-		vim.lsp.config("ts_ls", {
-			on_attach = on_attach,
-			capabilities = capabilities,
-			filetypes = {
-				"javascript",
-				"javascriptreact",
-				"typescript",
-				"typescriptreact",
-			},
-			root_dir = function(fname)
-				return vim.fs.dirname(
-					vim.fs.find({ "package.json", "tsconfig.json" }, { path = fname, upward = true })[1]
-				)
+		-- Add custom keymaps when LSP attaches
+		vim.api.nvim_create_autocmd("LspAttach", {
+			callback = function(args)
+				local bufnr = args.buf
+				local client = vim.lsp.get_client_by_id(args.data.client_id)
+				local tb = require("telescope.builtin")
+				local themes = require("telescope.themes")
+
+				-- Custom keymaps (in addition to Neovim 0.11+ defaults)
+				local map = function(mode, lhs, rhs, desc)
+					vim.keymap.set(mode, lhs, rhs, { buffer = bufnr, desc = desc })
+				end
+
+				-- Telescope LSP integration with <leader>l prefix
+				map("n", "<leader>ls", tb.lsp_document_symbols, "Document Symbols")
+				map("n", "<leader>lS", tb.lsp_workspace_symbols, "Workspace Symbols")
+				map("n", "<leader>lr", tb.lsp_references, "LSP References")
+				map("n", "<leader>li", tb.lsp_implementations, "LSP Implementations")
+				map("n", "<leader>lt", tb.lsp_type_definitions, "Type Definitions")
+				map("n", "<leader>lc", tb.lsp_incoming_calls, "Incoming Calls")
+				map("n", "<leader>lC", tb.lsp_outgoing_calls, "Outgoing Calls")
+
+				-- Diagnostics with dropdown theme
+				map("n", "<leader>ld", function()
+					tb.diagnostics(themes.get_dropdown({
+						previewer = false,
+						layout_config = { width = 0.7, height = 0.7 },
+					}))
+				end, "Diagnostics")
+
+				-- Buffer-specific diagnostics
+				map("n", "<leader>lD", function()
+					tb.diagnostics(themes.get_dropdown({
+						previewer = false,
+						layout_config = { width = 0.7, height = 0.7 },
+						bufnr = 0,
+					}))
+				end, "Buffer Diagnostics")
+
+				-- Additional LSP mappings
+				map("n", "<leader>la", vim.lsp.buf.code_action, "Code Action")
+				map("n", "<leader>ln", vim.lsp.buf.rename, "Rename Symbol")
+				map("n", "<leader>lh", vim.lsp.buf.signature_help, "Signature Help")
+				map("n", "<leader>lf", vim.lsp.buf.format, "Format Buffer")
+				map("n", "<leader>lq", vim.diagnostic.setloclist, "Diagnostics to Loclist")
+
+				-- Enable completion if supported
+				if client and client.supports_method("textDocument/completion") then
+					vim.lsp.completion.enable(true, client.id, bufnr, { autotrigger = true })
+				end
 			end,
 		})
-
-		vim.lsp.config("emmet_ls", {
-			on_attach = on_attach,
-			capabilities = capabilities,
-			filetypes = {
-				"html",
-				"css",
-				"scss",
-			},
-		})
-
-		vim.lsp.config("eslint", {
-			on_attach = on_attach,
-			capabilities = capabilities,
-		})
-
-		vim.lsp.config("cssls", {
-			on_attach = on_attach,
-			capabilities = capabilities,
-			filetypes = { "css" },
-		})
-
-		vim.lsp.config("bashls", {
-			on_attach = on_attach,
-			capabilities = capabilities,
-		})
-
-		vim.lsp.config("html", {
-			on_attach = on_attach,
-			capabilities = capabilities,
-		})
-
-		vim.lsp.config("systemd_ls", {
-			on_attach = on_attach,
-			capabilities = capabilities,
-		})
-
-		vim.lsp.config("pyright", {
-			on_attach = on_attach,
-			capabilities = capabilities,
-		})
-
-		vim.lsp.config("jsonls", {
-			on_attach = on_attach,
-			capabilities = capabilities,
-			filetypes = { "json", "jsonc" },
-		})
-
-		vim.lsp.config("css_variables", {
-			on_attach = on_attach,
-			capabilities = capabilities,
-			filetypes = { "css" },
-		})
-
-		vim.lsp.config("cssmodules_ls", {
-			on_attach = on_attach,
-			capabilities = capabilities,
-			filetypes = { "css" },
-		})
-
-		vim.lsp.enable("css_variables")
-		vim.lsp.enable("cssmodules_ls")
-		vim.lsp.enable("emmet_ls")
-		vim.lsp.enable("lua_ls")
-		vim.lsp.enable("rust_analyzer")
-		vim.lsp.enable("ts_ls")
-		vim.lsp.enable("eslint")
-		vim.lsp.enable("cssls")
-		vim.lsp.enable("bashls")
-		vim.lsp.enable("html")
-		vim.lsp.enable("systemd_ls")
-		vim.lsp.enable("pyright")
-		vim.lsp.enable("jsonls")
 	end,
 }
