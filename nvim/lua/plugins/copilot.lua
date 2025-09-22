5d9f373869b146910d8a619c55a1f7d54584e3fe
r4ppz <johnreyrabosa.f@gmail.com>
2025-09-22 12:35:38 +0800

    feat: add new copilot keybinds

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -7,6 +7,7 @@ return {
 	},
 	build = "make tiktoken",
 	opts = {
+		system_prompt = "Your name is Jarvis",
 		model = "gpt-4.1",
 		temperature = 0.1,
 		window = {
@@ -16,63 +17,33 @@ return {
 		auto_insert_mode = false,
 	},
 
-	config = function(_, opts)
-		require("CopilotChat").setup(opts)
-	end,
-
 	keys = {
-		-- Normal mode keybindings
-		{ "<leader>cc", "<cmd>CopilotChatToggle<cr>", desc = "Toggle CopilotChat" },
-
-		{ "<leader>cr", "<cmd>CopilotChatReset<cr>", desc = "Reset CopilotChat" },
-
-		-- Visual mode keybindings
-		{
-			"<leader>ce",
-			"<cmd>CopilotChatExplain<cr>",
-			mode = "v",
-			desc = "Explain selected code",
-		},
-
-		{ "<leader>cf", "<cmd>CopilotChatFix<cr>", mode = "v", desc = "Fix selected code" },
+		{ "<leader>ct", "<cmd>CopilotChatToggle<cr>", mode = { "n", "v" }, desc = "Toggle CopilotChat" },
+		{ "<leader>cr", "<cmd>CopilotChatReset<cr>", mode = "n", desc = "Reset CopilotChat" },
+		{ "<leader>cp", "<cmd>CopilotChatPrompts<cr>", mode = "n", desc = "View/select Prompt" },
 
 		{
-			"<leader>co",
-			"<cmd>CopilotChatOptimize<cr>",
-			mode = "v",
-			desc = "Optimize selected code",
-		},
-
-		{ "<leader>cr", "<cmd>CopilotChatReview<cr>", mode = "v", desc = "Review selected code" },
-
-		{
-			"<leader>cd",
-			"<cmd>CopilotChatDocs<cr>",
-			mode = "v",
-			desc = "Add docs to selected code",
-		},
-
-		{
-			"<leader>ct",
-			"<cmd>CopilotChatTests<cr>",
-			mode = "v",
-			desc = "Generate tests for selected code",
+			"<leader>ci",
+			function()
+				local chat = require("CopilotChat")
+				chat.open()
+				chat.chat:add_message({
+					role = "user",
+					content = "#buffer\n\n"
+						.. "Please review this code for idiomatic usage in [language/framework]. "
+						.. "Is it written in a way that aligns with common style, conventions, and best practices? "
+						.. "If so, point out which parts are idiomatic and why. "
+						.. "If not, explain what makes it non-idiomatic and provide concrete suggestions "
+						.. "to rewrite it in a more idiomatic way. "
+						.. "Include recommendations for readability, maintainability, and adherence to established conventions",
+				}, false)
+			end,
+			mode = { "n", "v" },
+			desc = "Check if code is idiomatic",
 		},
 
 		{
 			"<leader>cc",
-			function()
-				local select = require("CopilotChat.select")
-				require("CopilotChat").open({
-					selection = select.visual,
-				})
-			end,
-			mode = "v",
-			desc = "Open chat with selected code",
-		},
-
-		{
-			"<leader>cq",
 			function()
 				local chat = require("CopilotChat")
 				chat.open()
@@ -81,20 +52,35 @@ return {
 					content = "#buffer\n\n",
 				}, true)
 			end,
+			mode = { "n", "v" },
 			desc = "Open chat with current buffer",
 		},
 
 		{
-			"<leader>ca",
-			function()
-				local chat = require("CopilotChat")
-				chat.open()
-				chat.chat:add_message({
-					role = "user",
-					content = "#buffers\n\n",
-				}, true)
-			end,
-			desc = "Open chat with all open buffers",
+			"<leader>ce",
+			"<cmd>CopilotChatExplain<cr>",
+			mode = "v",
+			desc = "Explain selected code",
+		},
+		{ "<leader>cf", "<cmd>CopilotChatFix<cr>", mode = "v", desc = "Fix selected code" },
+		{
+			"<leader>co",
+			"<cmd>CopilotChatOptimize<cr>",
+			mode = "v",
+			desc = "Optimize selected code",
+		},
+		{ "<leader>cr", "<cmd>CopilotChatReview<cr>", mode = "v", desc = "Review selected code" },
+		{
+			"<leader>cd",
+			"<cmd>CopilotChatDocs<cr>",
+			mode = "v",
+			desc = "Add docs to selected code",
+		},
+		{
+			"<leader>ct",
+			"<cmd>CopilotChatTests<cr>",
+			mode = "v",
+			desc = "Generate tests for selected code",
 		},
 	},
 }
