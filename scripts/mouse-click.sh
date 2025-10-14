67b7f4e45aeda6a213eaba792bd691f5d21c8f49
r4ppz <johnreyrabosa.f@gmail.com>
2025-10-14 19:50:38 +0800

    feat(hypr): add Q/W/E/R mouse clicks and improve mouseclick script

--- scripts/mouse-click.sh
+++ scripts/mouse-click.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 set -euo pipefail
 
+STATE_FILE="/tmp/mouse_state"
+/usr/bin/ydotool click 0x80 2>/dev/null || true
+echo "released" >"$STATE_FILE"
+
 case "$1" in
 left) /usr/bin/ydotool click 0x40 0x80 ;;
 right) /usr/bin/ydotool click 0x41 0x81 ;;
 middle) /usr/bin/ydotool click 0x42 0x82 ;;
 toggle)
-  STATE_FILE="/tmp/mouse_state"
-
-  if [ ! -f "$STATE_FILE" ]; then
-    echo "released" >"$STATE_FILE"
-  fi
-
   CURRENT_STATE=$(cat "$STATE_FILE")
 
   if [ "$CURRENT_STATE" = "released" ]; then
