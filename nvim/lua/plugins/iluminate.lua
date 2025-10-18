a64033b5dad180d9bc8a89a288611ade5a4dad7c
r4ppz <johnreyrabosa.f@gmail.com>
2025-10-19 01:34:13 +0800

    feat(plugins): add vim-illuminate for enhanced symbol highlighting

---
+++ nvim/lua/plugins/iluminate.lua
@@ -0,0 +1,47 @@
+return {
+  "RRethy/vim-illuminate",
+  event = "BufRead",
+  config = function()
+    require("illuminate").configure {
+      providers = { "lsp", "treesitter", "regex" },
+      delay = 50,
+      under_cursor = true,
+
+      large_file_cutoff = 5000,
+      large_file_overrides = {
+        under_cursor = false,
+        providers = { "regex" },
+        min_count_to_highlight = 2,
+      },
+
+      filetypes_denylist = {
+        "NvimTree",
+        "TelescopePrompt",
+        "qf",
+        "help",
+        "man",
+        "terminal",
+        "dirbuf",
+        "fugitive",
+        "copilot-chat",
+      },
+
+      modes_denylist = { "i" },
+      providers_regex_syntax_denylist = { "Comment", "String", "Constant" },
+      min_count_to_highlight = 2,
+
+      disable_keymaps = false,
+    }
+
+    vim.keymap.set("n", "gn", require("illuminate").goto_next_reference, { desc = "Next Reference" })
+    vim.keymap.set("n", "gn", require("illuminate").goto_prev_reference, { desc = "Prev Reference" })
+    vim.keymap.set("o", "ir", require("illuminate").textobj_select, { desc = "Select Reference" })
+
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = "terminal",
+      callback = function()
+        require("illuminate").pause_buf()
+      end,
+    })
+  end,
+}
