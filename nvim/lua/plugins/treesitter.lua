25f73d1ccc7f04a4090577dbdd57c8438933e395
r4ppz <johnreyrabosa.f@gmail.com>
2025-11-14 17:18:01 +0800

    feat(treesitter): transition to main branch
    - idk if the config is correct, but it works ?

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -2,7 +2,6 @@ return {
   "nvim-treesitter/nvim-treesitter",
   branch = "main",
   lazy = false,
-  build = ":TSUpdate",
   opts = {
     ensure_installed = {
       "luadoc",
@@ -13,12 +12,12 @@ return {
       "markdown_inline",
       "query",
 
+      "sql",
       "lua",
       "bash",
       "java",
       "rust",
       "python",
-      "sql",
       "c",
       "cpp",
       "hyprlang",
@@ -32,6 +31,8 @@ return {
       "css",
       "javascript",
       "typescript",
+      "typescriptreact",
+      "javascriptreact",
       "tsx",
 
       "diff",
@@ -41,11 +42,25 @@ return {
       "gitignore",
       "gitattributes",
     },
-
-    highlight = {
-      enable = true,
-      additional_vim_regex_highlighting = false,
-    },
-    indent = { enable = true },
   },
+  config = function(_, opts)
+    require('nvim-treesitter').setup()
+
+    local ensure_installed = opts.ensure_installed
+    if ensure_installed and #ensure_installed > 0 then
+      require('nvim-treesitter').install(ensure_installed)
+    end
+
+    vim.api.nvim_create_autocmd('FileType', {
+      group = vim.api.nvim_create_augroup('TreesitterEnable', { clear = true }),
+      pattern = opts.ensure_installed,
+      callback = function()
+        local bufnr = 0
+        pcall(vim.treesitter.start, bufnr)
+        if vim.bo[bufnr].indentkeys == '' then
+          vim.bo[bufnr].indentexpr = 'v:lua.require"nvim-treesitter".indentexpr()'
+        end
+      end,
+    })
+  end,
 }
