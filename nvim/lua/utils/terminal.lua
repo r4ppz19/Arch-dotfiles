c61888e169c71fb7b5f4127e35f27a189a5ecf3d
r4ppz <johnreyrabosa.f@gmail.com>
2025-11-28 12:36:12 +0800

    refactor: inline get_tab_id and include position in terminal ID

--- nvim/lua/utils/terminal.lua
+++ nvim/lua/utils/terminal.lua
@@ -1,9 +1,5 @@
 local M = {}
 
-local function get_tab_id()
-  return "tab_term_" .. tostring(vim.api.nvim_get_current_tabpage())
-end
-
 M.toggle_tab_terminal = function(opts)
   local default_opts = {
     pos = "float",
@@ -11,7 +7,7 @@ M.toggle_tab_terminal = function(opts)
     direction = "horizontal",
   }
   local config = vim.tbl_extend("force", default_opts, opts or {})
-  config.id = get_tab_id()
+  config.id = "tab_term_" .. tostring(vim.api.nvim_get_current_tabpage()) .. "_" .. config.pos
   require("nvchad.term").toggle(config)
 end
 
