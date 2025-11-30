f900ede30658b86715812bf412d1c4bc2f40490f
r4ppz <johnreyrabosa.f@gmail.com>
2025-11-30 10:50:07 +0800

    fix(copilot): old buffer references commands

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -64,7 +64,7 @@ return {
       function()
         local chat = require("CopilotChat")
         chat.open()
-        chat.chat:add_message({ role = "user", content = "#buffer\n" }, true)
+        chat.chat:add_message({ role = "user", content = "#buffer:active\n" }, true)
       end,
       mode = { "n", "v" },
       desc = "Open chat with current buffer",
@@ -76,7 +76,7 @@ return {
       function()
         local chat = require("CopilotChat")
         chat.open()
-        chat.chat:add_message({ role = "user", content = "#buffers\n" }, true)
+        chat.chat:add_message({ role = "user", content = "#buffer:listed\n" }, true)
       end,
       mode = { "n", "v" },
       desc = "Open chat with all buffers",
