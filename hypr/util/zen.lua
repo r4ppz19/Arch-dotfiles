eeffbb73a3385db6d2ba144b534f9ea5b49dc0b8
r4ppz <johnreyrabosa.f@gmail.com>
2026-06-01 13:30:23 +0800

    fix(zen): adjust general/normal config to match with default general
    Update submodule

--- hypr/util/zen.lua
+++ hypr/util/zen.lua
@@ -6,7 +6,7 @@ local zen = false
 
 local config = {
   normal = {
-    general = { gaps_out = 15, gaps_in = 8, border_size = 2 },
+    general = { gaps_in = 10, gaps_out = 20 },
     decoration = { rounding = 1, rounding_power = 5 },
     wallpaper = os.getenv("DOTFILES") .. "/wallpaper/wallpaper2.png",
     waybar = "start",
