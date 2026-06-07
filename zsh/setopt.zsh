c083b122f31c6db0bc553587ecec0d5eb7f84779
r4ppz <johnreyrabosa.f@gmail.com>
2026-06-07 09:29:04 +0800

    chore(zsh): add new setopt for optimizing history

--- zsh/setopt.zsh
+++ zsh/setopt.zsh
@@ -1,10 +1,12 @@
 # History Setup
-setopt append_history    # Append history logs instead of overwriting the target file
-setopt extended_history  # Add precise Unix timestamps per logged entry
-setopt hist_ignore_space # Completely ignore commands starting with a literal space character
-setopt hist_verify       # Load recalled commands into the active prompt buffer for editing before execution
+setopt APPEND_HISTORY       # Append history logs instead of overwriting the target file
+setopt EXTENDED_HISTORY     # Add precise Unix timestamps per logged entry
+setopt HIST_VERIFY          # Load recalled commands into the active prompt buffer for editing before execution
+setopt HIST_IGNORE_SPACE    # Completely ignore commands starting with a literal space character
+setopt HIST_IGNORE_DUPS     # Do not write a command to the history file if it duplicates the previous one
+setopt HIST_IGNORE_ALL_DUPS # If a new command duplicates an older one, remove the older one from the history list
 
 # Shell Operations
-setopt interactive_comments # Allow `#` to act as an execution comment interactively
-setopt extended_glob        # Enable powerful pattern matching features (`^`, `~`, `**/`)
-setopt notify               # Immediately broadcast status updates for background processes
+setopt INTERACTIVE_COMMENTS # Allow `#` to act as an execution comment interactively
+setopt EXTENDED_GLOB        # Enable powerful pattern matching features (`^`, `~`, `**/`)
+setopt NOTIFY               # Immediately broadcast status updates for background processes
