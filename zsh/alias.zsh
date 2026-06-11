6a85bded72f3968929f29d52463c94d8fe18fe88
r4ppz <johnreyrabosa.f@gmail.com>
2026-06-11 11:01:26 +0800

    chore: update pkglist and alias

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -25,7 +25,7 @@ alias ai='opencode run'
 alias md='glow'
 alias pac='pacseek'
 alias copy='wl-copy'
-alias blue='bluetui'
+alias blue='bluetuith'
 
 # services
 alias smbon='sudo systemctl start smb.service nmb.service && echo "Samba server started"'
