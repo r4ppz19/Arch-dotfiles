5034ec8cc25b46e64effb215ab7c06a7942cc2ab
r4ppz <johnreyrabosa.f@gmail.com>
2026-07-20 18:33:25 +0800

    refactor(autostart): migrate to systemd services

--- hypr/core/autostart.lua
+++ hypr/core/autostart.lua
@@ -1,8 +1,6 @@
 hl.on("hyprland.start", function()
   hl.exec_cmd("systemctl --user start hyprland.target")
   hl.exec_cmd("dbus-update-activation-environment --systemd --all")
-  hl.exec_cmd("nm-applet --indicator")
-  hl.exec_cmd("blueman-applet")
 end)
 
 hl.on("hyprland.shutdown", function()
